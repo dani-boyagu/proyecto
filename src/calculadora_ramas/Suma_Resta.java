3632f299d3550157eababfbcd7d9ec933b21d170
usuario <usuario@usuario-PC>
2019-04-02 11:47:52 +0200

    Suma y Resta

---
+++ src/calculadora_ramas/Suma_Resta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora_ramas;
+
+/**
+ *
+ * @author usuario
+ */
+public class Suma_Resta {
+    
+    private int num1, num2,result;
+
+    public Suma_Resta() {
+    }
+    
+    public int Suma(){
+        result=num1+num2;
+        return result;
+    }
+    public int Resta(){
+        result=num1-num2;
+        return result;
+    }
+}
