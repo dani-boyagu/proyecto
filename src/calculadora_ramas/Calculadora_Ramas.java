1d4db8d8234cce29be42cd10a0d98e0b459da11a
usuario <usuario@usuario-PC>
2019-04-24 09:07:13 +0200

    Main Prueba con otra rama

---
+++ src/calculadora_ramas/Calculadora_Ramas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora_ramas;
+
+/**
+ *
+ * @author usuario
+ */
+public class Calculadora_Ramas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Suma_Resta a=new Suma_Resta();
+        
+        System.out.println(a.Resta());
+        System.out.println(a.Suma());
+    }
+    
+}
